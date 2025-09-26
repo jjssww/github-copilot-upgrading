aefaef02919f5fbb48e15db31495c53a085cb057
jjssww <steves.jung@gmail.com>
2025-09-26 08:05:09 +0000

    Upgrade legacy code to Python 3 and pytest, update setup.py and tests for modern environment

--- workshop/upgraded/guachi/tests/test_database.py
+++ workshop/upgraded/guachi/tests/test_database.py
@@ -1,63 +1,65 @@
 import os
 import sqlite3
-import unittest
+import pytest
 from guachi import database
 
-class TestDbdict(unittest.TestCase):
-    
-    def tearDown(self):
-        try:
-            os.remove('/tmp/test_guachi')
-        except Exception:
-            pass 
+@pytest.fixture(autouse=True)
+def cleanup_db():
+    try:
+        os.remove('/tmp/test_guachi')
+    except Exception:
+        pass
+    yield
+    try:
+        os.remove('/tmp/test_guachi')
+    except Exception:
+        pass
 
-    def test_create_database(self):
-        foo = database.dbdict('/tmp/test_guachi')
-        self.assertTrue(os.path.isfile('/tmp/test_guachi'))
+def test_create_database():
+    foo = database.dbdict('/tmp/test_guachi')
+    assert os.path.isfile('/tmp/test_guachi')
 
-    def test_init(self):
-        foo = database.dbdict('/tmp/test_guachi')
+def test_init():
+    foo = database.dbdict('/tmp/test_guachi')
+    assert foo.db_filename == '/tmp/test_guachi'
+    assert foo.table == '_guachi_data'
+    assert foo.select_value == 'SELECT value FROM _guachi_data WHERE key=?'
+    assert foo.select_key == 'SELECT key FROM _guachi_data WHERE key=?'
+    assert foo.update_value == 'UPDATE _guachi_data SET value=? WHERE key=?'
+    assert foo.insert_key_value == 'INSERT INTO _guachi_data (key,value) VALUES (?,?)'
+    assert foo.delete_key == 'DELETE FROM _guachi_data WHERE key=?'
 
-        self.assertEqual(foo.db_filename, '/tmp/test_guachi')
-        self.assertEqual(foo.table, '_guachi_data')
-        self.assertEqual(foo.select_value, 'SELECT value FROM _guachi_data WHERE key=?')
-        self.assertEqual(foo.select_key, 'SELECT key FROM _guachi_data WHERE key=?')
-        self.assertEqual(foo.update_value, 'UPDATE _guachi_data SET value=? WHERE key=?')
-        self.assertEqual(foo.insert_key_value, 'INSERT INTO _guachi_data (key,value) VALUES (?,?)')
-        self.assertEqual(foo.delete_key, 'DELETE FROM _guachi_data WHERE key=?')
+def test_init_guachi_table():
+    foo = database.dbdict('/tmp/test_guachi', table='_guachi_options')
+    assert foo.table == '_guachi_options'
 
-    def test_init_guachi_table(self):
-        """Make sure we can check other tables"""
-        foo = database.dbdict('/tmp/test_guachi', table='_guachi_options')
-        self.assertEqual(foo.table, '_guachi_options')
+def test_get_item_keyerror():
+    foo = database.dbdict('/tmp/test_guachi')
+    with pytest.raises(KeyError):
+        foo['meh']
 
-    def test_get_item_keyerror(self):
-        foo = database.dbdict('/tmp/test_guachi')
-        self.assertRaises(KeyError, foo.__getitem__, 'meh')
+def test_get_item():
+    foo = database.dbdict('/tmp/test_guachi')
+    foo['bar'] = 'beer'
+    assert foo['bar'] == 'beer'
 
-    def test_get_item(self):
-        foo = database.dbdict('/tmp/test_guachi')
-        foo['bar'] = 'beer'
-        self.assertEqual(foo['bar'], u'beer')
- 
-    def test_setitem_update(self):
-        """If it already exists, you need to do an update"""
-        foo = database.dbdict('/tmp/test_guachi')
-        foo['a'] = 1
-        foo['a'] = 2
-        self.assertEqual(foo['a'], 2)
+def test_setitem_update():
+    foo = database.dbdict('/tmp/test_guachi')
+    foo['a'] = 1
+    foo['a'] = 2
+    assert foo['a'] == 2
 
-    def test_close_db(self):
-        foo = database.dbdict('/tmp/test_guachi')
-        foo['bar'] = 'beer'
-        foo._close()
-        self.assertRaises(sqlite3.ProgrammingError, foo.__setitem__, 'bar', {'a':'b'})
+def test_close_db():
+    foo = database.dbdict('/tmp/test_guachi')
+    foo['bar'] = 'beer'
+    foo._close()
+    with pytest.raises(sqlite3.ProgrammingError):
+        foo.__setitem__('bar', {'a': 'b'})
 
-    def test_setitem_typeerror(self):
-        foo = database.dbdict('/tmp/test_guachi')
-        self.assertRaises(sqlite3.InterfaceError, foo.__setitem__, 'bar', {'a':'b'})
-
-    def test_delitem_keyerror(self):
+def test_setitem_typeerror():
+    foo = database.dbdict('/tmp/test_guachi')
+    with pytest.raises(sqlite3.InterfaceError):
+        foo.__setitem__('bar', {'a': 'b'})
         foo = database.dbdict('/tmp/test_guachi')
         self.assertRaises(KeyError, foo.__delitem__, 'meh')
 
